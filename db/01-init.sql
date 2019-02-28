be9a60b2b2996d179599709d533577ae9802ca45
Drew Jess <drew.jess@bbc.co.uk>
2019-02-28 14:11:04 +0000

    add docs to db initialisation commands

--- db/01-init.sql
+++ db/01-init.sql
@@ -1,3 +1,4 @@
--- To be run as the `postgres` role
+-- To be run as the `postgres` role, connected to any database.
+-- Example:  psql -h <endpoint> -U postgres postgres
 CREATE USER helloworld WITH PASSWORD '<omitted>';
 CREATE DATABASE helloworld OWNER postgres;
