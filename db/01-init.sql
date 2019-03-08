5fe36ffe02386f90d169e6872172fd1d152cbc0c
Drew Jess <drew.jess@bbc.co.uk>
2019-03-08 11:44:23 +0000

    reorg

--- db/01-init.sql
+++
@@ -1,4 +0,0 @@
--- To be run as the `postgres` role, connected to any database.
--- Example:  psql -h <endpoint> -U postgres postgres
-CREATE USER helloworld WITH PASSWORD '<omitted>';
-CREATE DATABASE helloworld OWNER postgres;
--- db/01-schema.sql
+++ db/01-schema.sql
@@ -1,5 +1,23 @@
 -- To be run as the `postgres` role, connected to the `helloworld` database
 -- Example:  psql -h <endpoint> -U postgres helloworld
+CREATE DATABASE helloworld OWNER postgres;
+CREATE USER helloworld WITH PASSWORD '<omitted>';
+
+\c helloworld
+
+CREATE SCHEMA helloworld;
+DROP SCHEMA public;
+
+CREATE TABLE IF NOT EXISTS helloworld.foo (
+  id    SERIAL PRIMARY KEY,
+  name  varchar(40) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS helloworld.bar (
+  id       SERIAL PRIMARY KEY,
+  species  varchar(40) NOT NULL
+);
+
 GRANT CONNECT ON DATABASE helloworld TO helloworld;
 GRANT CONNECT ON DATABASE helloworld TO helloworld;
 GRANT SELECT, UPDATE, INSERT, DELETE ON helloworld.foo TO helloworld;
